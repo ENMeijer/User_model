49b6f6fb498285e2395034908652bd0e22913fa2
ifeelgitty <42966018+ifeelgitty@users.noreply.github.com>
2019-09-18 22:27:00 +0200

    Basic App + Notification
    Main Menu Screen + Selection of Task Screen (so far simplified of course) When pressing "Settings", the app shows a notification!
    All of this mainly to implement functionality which later will ofc be adjusted properly

---
+++ CramDroid/app/src/main/java/com/example/cramdroid/TaskChoiceActivity.kt
@@ -0,0 +1,18 @@
+package com.example.cramdroid
+
+import android.app.NotificationManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.NotificationManagerCompat
+
+class TaskChoiceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_task_choice)
+
+
+    }
+
+
+}
